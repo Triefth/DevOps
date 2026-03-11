4476103a39d21cd7455e8dbb5fd4dbc7fa0f51b3
Triefth <da.cifuentesh@duocuc.cl>
2026-03-11 17:18:25 -0300

    Update PlanController.java

--- src/main/backend/controller/PlanController.java
+++ src/main/backend/controller/PlanController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/planes")
-@CrossOrigin(origins = "http://localhost:3000") 
+@CrossOrigin("*") 
 public class PlanController {
 
     @Autowired
