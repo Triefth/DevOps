223297bec67374cc38dd7d83aba938388e4bb509
Triefth <da.cifuentesh@duocuc.cl>
2026-03-18 16:43:13 -0300

    coso

--- src/main/backend/repository/PlanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devops.repository;
-import com.devops.model.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
